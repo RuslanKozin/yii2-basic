42d2a78bf51c09089e0b61eb0626bc09c4768297
RuslanKozin <ruslan3723@mail.ru>
2016-08-20 17:02:26 +0300

    Модульная структура №2
    Перенес и изменил файлы

---
+++ modules/main/controllers/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\modules\main\controllers;
+
+use app\modules\main\models\ContactForm;
+use yii\web\Controller;
+use Yii;
+
+/**
+ * Default controller for the `main` module
+ */
+class ContactController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actions()
+    {
+        return [
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        } else {
+            return $this->render('index', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
