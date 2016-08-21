081a5b3c63bd38287cec076f8001c94a4886b8e5
RuslanKozin <ruslan3723@mail.ru>
2016-08-21 14:57:46 +0300

    Перенос пользователей в БД №5
    Добавляем IdentityInterface, чтобы хранить авторизованного пользователя.
    Добавляем модели findByUsername и validatePassword для работы класса LoginForm.
    Для хэширования паролей будем использовать новый компонент Security.

--- modules/user/models/User.php
+++ modules/user/models/User.php
@@ -7,6 +7,8 @@ use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
+
 
 
 /**
@@ -93,4 +95,52 @@ class User extends \yii\db\ActiveRecord
             TimestampBehavior::className(),
         ];
     }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('findIdentityByAccessToken is not implemented.');
+    }
+
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
 }
